73956e5edeb7a4947befd41672d5bceb76764ee1
ValeyaW <weyvalentina@gmail.com>
2021-11-09 13:30:02 +0100

    changed a lot

--- AirportScheduler/src/ch/modul226/airport/model/InvalidGateNumberException.java
+++
@@ -1,8 +0,0 @@
-package ch.modul226.airport.model;
-
-public class InvalidGateNumberException extends Exception {
-
-    public InvalidGateNumberException(){
-        super("Sorry- Diese Gatenummer gibt es nicht");
-    }
-}
--- AirportScheduler/src/ch/modul226/airport/model/NoGateAvailableException.java
+++
@@ -1,8 +0,0 @@
-package ch.modul226.airport.model;
-
-public class NoGateAvailableException extends Exception {
-
-    public NoGateAvailableException(){
-        super("Sorry- Kein freies Gate gefunden");
-    }
-}
